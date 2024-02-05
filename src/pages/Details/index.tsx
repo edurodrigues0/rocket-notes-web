129a83ceb4915d59a7563e46f4e6e83e9ded0b0e
edurodrigues0 <edusacramento9@gmail.com>
2024-02-05 20:40:47 -0300

    feat: adicionando marcadores

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,17 +1,24 @@
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 import { Container, Links } from './styles'
 
 export function Details() {
   return (
     <Container>
       <Header />
+
       <Section title="Links úteis">
         <Links>
           <li>
             <a href="#">Link 1</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
     </Container>
   )
