3bd154c96384c3617182db36d51912efa3e15e57
edurodrigues0 <edusacramento9@gmail.com>
2024-02-05 19:16:31 -0300

    feat: adicionando tema global

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.ts'
+import GlobalStyles from './styles/global.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
