138c2203203cb085d0864ad0993cd8b3c7f43c72
edurodrigues0 <edusacramento9@gmail.com>
2024-02-19 15:13:05 -0300

    feat: Adicionando pagina de SignUp

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,32 @@
+import { Background, Container, Form } from './styles'
+import { Input } from '../../components/Input'
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Button } from '../../components/Button'
+
+export function SignUp() {
+  return (
+    <Container>
+      <Background />
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicacao para salvar e gerenciar seus links uteis.</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input placeholder="Nome" type="text" icon={<FiUser size={20} />} />
+
+        <Input placeholder="E-mail" type="text" icon={<FiMail size={20} />} />
+
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={<FiLock size={20} />}
+        />
+
+        <Button title="Cadastrar" />
+
+        <a href="#">Voltar para o login</a>
+      </Form>
+    </Container>
+  )
+}
